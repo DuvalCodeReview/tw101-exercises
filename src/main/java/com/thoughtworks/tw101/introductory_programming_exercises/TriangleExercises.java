94d2415fdd7d2894b54031365fedb00441562da3
dlanell <dsmith.nccu14@gmail.com>
2017-07-03 01:00:57 -0400

    Triangle Exercise Solution
    code for TriangleExercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
@@ -4,22 +4,28 @@ public class TriangleExercises {
     public static void main(String[] args) {
         drawAnAsterisk();
         drawAHorizontalLine(8);
-        drawAVerticalLine(3);
-        drawARightTriangle(3);
+        drawAVerticalLine(5);
+        drawARightTriangle(5);
     }
 
 //    Easiest exercise ever
 //    Print one asterisk to the console.
 //    Example: *
     private static void drawAnAsterisk() {
-
+        System.out.println("drawAnAsterisk call: ");
+        System.out.print("*");
+        System.out.println("\n------------------------------------");
     }
 
 //    Draw a horizontal line
 //    Given a number n, print n asterisks on one line.
 //    Example when n=8:  ********
     private static void drawAHorizontalLine(int n) {
-
+        System.out.println("drawAHorizontalLine call: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println("\n------------------------------------");
     }
 
 //    Draw a vertical line
@@ -29,7 +35,11 @@ public class TriangleExercises {
 //            *
 //            *
     private static void drawAVerticalLine(int n) {
-
+        System.out.println("drawAVerticalLine call: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+        System.out.println("------------------------------------");
     }
 
 //    Draw a right triangle
@@ -39,6 +49,13 @@ public class TriangleExercises {
 //            **
 //            ***
     private static void drawARightTriangle(int n) {
-
+        System.out.println("drawARightTriangle call: ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------");
     }
 }
