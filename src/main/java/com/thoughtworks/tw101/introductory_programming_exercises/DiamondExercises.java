863fea45409321fe24368bcd04869bb8f871a2e0
Salvador Ariza <salvadorariza94@gmail.com>
2017-07-19 21:13:53 -0500

    Reformatted drawADiamond... An interesting note
    It seems that drawADiamond has duplicate code from drawAnIsoscelesTriangle()

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -3,7 +3,7 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
-        drawADiamond(8);
+        drawADiamond(3);
         drawADiamondWithYourName(3);
     }
 
@@ -41,35 +41,30 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-        int stars = 1, spaces = n + 1, rowCount = 1;
+        int stars = 1, spaces = n + 1;
 
         System.out.println("drawADiamond call: ");
 
-        for (int row = 1; row <= n+(n-1); row++) {
-
-            if(rowCount < n) {
-                for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
-                    System.out.print(" ");
-                }
-                for (int starCount = 1; starCount <= stars; starCount++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-                stars += 2;
-                spaces -= 1;
-                rowCount += 1;
-            } else {
-                for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
-                    System.out.print(" ");
-                }
-                for (int starCount = 1; starCount <= stars; starCount++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-                stars -= 2;
-                spaces += 1;
-                rowCount += 1;
+        for (int rowCount = 1; rowCount < n; rowCount++, stars+=2, spaces-=1) {
+            for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
+                System.out.print(" ");
             }
+            for (int starCount = 1; starCount <= stars; starCount++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int row = 1; row <= n; row++) {
+            for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
+                System.out.print(" ");
+            }
+            for (int starCount = 1; starCount <= stars; starCount++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            stars -= 2;
+            spaces += 1;
         }
 
         System.out.println("------------------------------------");
