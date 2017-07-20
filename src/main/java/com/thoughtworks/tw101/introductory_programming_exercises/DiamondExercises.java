1060fdffd5ebdc2c35a82439a6316275cee555b8
Salvador Ariza <salvadorariza94@gmail.com>
2017-07-19 22:00:57 -0500

    drawADiamondWithName: extracted remaining dup code to existing methods

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -49,7 +49,6 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
         drawAnIsoscelesTriangle(n-1);
         int stars = (2*n)-1;  //1 + 2(n-1) (we need the stars and spaces info  to carry over)
         int spaces = 1;       //n - 1
@@ -59,7 +58,6 @@ public class DiamondExercises {
             drawStars(stars);
             System.out.println();
         }
-
     }
 
 //    Diamond with Name
@@ -76,28 +74,19 @@ public class DiamondExercises {
         for (int row = 1; row <= n+(n-1); row++) {
 
             if(rowCount < n) {
-                for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
-                    System.out.print(" ");
-                }
-
-                for (int starCount = 1; starCount <= stars; starCount++) {
-                    System.out.print("*");
-                }
+                drawSpaces(spaces);
+                drawStars(stars);
 
                 System.out.println();
                 stars += 2;
                 spaces -= 1;
                 rowCount += 1;
             } else {
-                for (int spaceCount = 1; spaceCount <= spaces; spaceCount++) {
-                    System.out.print(" ");
-                }
+                drawSpaces(spaces);
                 if (rowCount == (n)) {
                     System.out.print("Duvall");
                 } else {
-                    for (int starCount = 1; starCount <= stars; starCount++) {
-                        System.out.print("*");
-                    }
+                    drawStars(stars);
                 }
                 System.out.println();
                 stars -= 2;
